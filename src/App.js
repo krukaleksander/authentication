d51b3497eee04fa37eb29c2f1532cee74cfa8884
Alek Kruk <kruk.aleksander@hotmail.com>
2021-01-31 09:34:17 +0100

    creating user fine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Signup from './components/Signup';
 import { Container } from 'react-bootstrap';
+import AuthProvider from './context/AuthContext';
 const App = () => {
   return (
-    <Container className='d-flex align-items-center justify-content-center' style={{ minHeight: '100vh' }}>
-      <div className="w-100" style={{ maxWidth: '400px' }}>
-        <Signup />
-      </div>
-    </Container>
+    <AuthProvider>
+      <Container className='d-flex align-items-center justify-content-center' style={{ minHeight: '100vh' }}>
+        <div className="w-100" style={{ maxWidth: '400px' }}>
+          <Signup />
+        </div>
+      </Container>
+    </AuthProvider>
+
+
   )
 
 
