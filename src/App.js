7d885e837e2c167543857096f82b5be7fea92a33
Alek Kruk <kruk.aleksander@hotmail.com>
2021-01-31 09:52:18 +0100

    add react-router-dom, create login component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import Signup from './components/Signup';
 import { Container } from 'react-bootstrap';
 import AuthProvider from './context/AuthContext';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
 const App = () => {
   return (
-    <AuthProvider>
-      <Container className='d-flex align-items-center justify-content-center' style={{ minHeight: '100vh' }}>
-        <div className="w-100" style={{ maxWidth: '400px' }}>
-          <Signup />
-        </div>
-      </Container>
-    </AuthProvider>
+
+    <Container className='d-flex align-items-center justify-content-center' style={{ minHeight: '100vh' }}>
+      <div className="w-100" style={{ maxWidth: '400px' }}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route path='/signup' component={Signup} />
+              <Route path='/login' component={Login} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
+
 
 
   )
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,11 +2,9 @@ import React, { useRef, useState } from 'react';
 import { Form, Button, Card, Alert } from 'react-bootstrap';
 import { useAuth } from '../context/AuthContext';
 import { Link } from 'react-router-dom';
-
-export default function Signup() {
+export default function Login() {
     const emailRef = useRef();
     const passwordRef = useRef();
-    const passwordConfirmRef = useRef();
     const signup = useAuth();
     const [error, setError] = useState('');
     const [loading, setLoading] = useState(false);
@@ -14,9 +12,6 @@ export default function Signup() {
     async function handleSubmit(e) {
         e.preventDefault();
 
-        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-            return setError('Passwords do not match');
-        }
         try {
             setError('');
             setLoading(true);
@@ -31,7 +26,7 @@ export default function Signup() {
         <>
             <Card>
                 <Card.Body>
-                    <h2 className="text-center mb-4">Sign up</h2>
+                    <h2 className="text-center mb-4">Log in</h2>
                     {error && <Alert variant='danger'>{error}</Alert>}
                     <Form onSubmit={handleSubmit}>
                         <Form.Group id='email'>
@@ -42,16 +37,12 @@ export default function Signup() {
                             <Form.Label>Password</Form.Label>
                             <Form.Control type='password' required ref={passwordRef} />
                         </Form.Group>
-                        <Form.Group id='passwordConfirm'>
-                            <Form.Label>Password Confirmation</Form.Label>
-                            <Form.Control type='password' required ref={passwordConfirmRef} />
-                        </Form.Group>
-                        <Button disabled={loading} className='w-100' type='submit'>Sign up</Button>
+                        <Button disabled={loading} className='w-100' type='submit'>Log In</Button>
                     </Form>
                 </Card.Body>
             </Card>
             <div className='w-100 text-center mt-2'>
-                Already have an account? <Link to='/login'>Log in</Link>
+                Need an account? <Link to='/signup'>Sign up</Link>
             </div>
         </>
     )
